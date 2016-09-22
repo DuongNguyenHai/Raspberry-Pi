a0558d85f72797db05d880c1bd4568a5f78f2504
Nguyen Hai Duong <duongnguyenhai.hust@gmail.com>
2016-09-22 16:31:23 +0700

    move sql-function to sql forder

---
+++ Tutorial/web-server/lazy/sql/sql-define.php
@@ -0,0 +1,9 @@
+<?php 
+
+define("serverName", "localhost");
+define("userName", "root");
+define("password", "nightsky");
+define("dbName", "home");
+define("TABLE", " device "); // need space between 'device'
+
+?>
